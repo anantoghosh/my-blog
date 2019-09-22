040cc47d565d46beb94e4e83a2cf88e64a0fb313
Ananto Ghosh <a0ghosh@gmail.com>
2019-09-22 18:10:21 +0530

    Add mobile nav

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import Headroom from 'react-headroom';
 import logo from '../../static/logo/logo.png';
 
 import styles from './NavBar.module.scss';
 
-const NavBar = () => (
-  <Headroom calcHeightOnResize disableInlineStyles>
-    <Link to="/" className={styles.link}>
-      <img src={logo} alt="Gatsby Logo" />
-    </Link>
-    <nav className={styles.nav}>
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
-      {/* <Link to="/work">Work</Link> */}
-      <Link to="/contact">Contact</Link>
-    </nav>
-  </Headroom>
-);
+const NavBar = () => {
+  let [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <Headroom calcHeightOnResize disableInlineStyles>
+        <Link to="/" className={styles.link}>
+          <img src={logo} alt="Gatsby Logo" />
+        </Link>
+        <button
+          className={`${styles.menu} ${isOpen ? styles.in : ''}`}
+          onClick={() => setIsOpen(true)}
+        >
+          ☰
+        </button>
+        <nav className={`${styles.nav} ${isOpen ? styles.hidden : ''}`}>
+          <Link to="/">Home</Link>
+          <Link to="/blog">Blog</Link>
+          {/* <Link to="/work">Work</Link> */}
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </Headroom>
+      <nav className={`${styles.sidebar} ${isOpen ? styles.in : ''}`}>
+        <button
+          className={`${styles.close} ${isOpen ? styles.in : ''}`}
+          onClick={() => setIsOpen(false)}
+        >
+          Close X
+        </button>
+        <Link to="/">Home</Link>
+        <Link to="/blog">Blog</Link>
+        {/* <Link to="/work">Work</Link> */}
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </>
+  );
+};
 
 export default NavBar;
